039d592fc5dfef76918c166a5bfd1a764e3ff978
Md.Harun Or Rashid <horrashid007@gmail.com>
2019-10-31 20:07:32 +0600

    change config variable of database

--- server.js
+++ server.js
@@ -4,11 +4,7 @@ const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
 
-const DB = process.env.DATABASE.replace(
-  '<PASSWORD>',
-  process.env.DATABASE_PASSWORD
-);
-
+const DB = process.env.DATABASE;
 //connect database
 mongoose
   .connect(DB, {
