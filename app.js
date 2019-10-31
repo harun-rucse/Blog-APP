09dbc6c6854100ee313aa1e7a408937682b1a724
Md.Harun Or Rashid <horrashid007@gmail.com>
2019-10-31 15:08:41 +0600

    create a user schema and model for CRUD operation and also define user route

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ dotenv.config({ path: './config.env' });
 
 const app = express();
 //Middleware
+app.use(express.json());
 app.use(morgan('dev'));
 
 const DB = process.env.DATABASE.replace(
@@ -24,10 +25,62 @@ mongoose
   .then(() => console.log('DB connect successful!'))
   .catch(err => console.log('DB connection failed!'));
 
-app.get('/api/v1/users', (req, res) => {
-  res.status(200).json({
-    status: 'success'
-  });
+//Create schema for CRUD operations
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    require: [true, 'use must have a name']
+  },
+  password: {
+    type: String,
+    require: [true, 'use must have a password']
+  },
+  passwordConfirm: {
+    type: String,
+    require: true
+  },
+  role: {
+    type: String,
+    default: 'user'
+  }
+});
+
+//craete model
+const User = mongoose.model('User', userSchema);
+
+app.get('/api/v1/users', async (req, res) => {
+  try {
+    const allUsers = await User.find();
+
+    res.status(200).json({
+      status: 'success',
+      result: allUsers.length,
+      data: {
+        users: allUsers
+      }
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err
+    });
+  }
+});
+app.post('/api/v1/users', async (req, res) => {
+  try {
+    const newUser = await User.create(req.body);
+    res.status(200).json({
+      status: 'success',
+      data: {
+        newUser
+      }
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err
+    });
+  }
 });
 
 //Start the server
