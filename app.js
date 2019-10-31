300f1fb44c13f01556c43b2a74ad63bbde5eda7b
Md.Harun Or Rashid <horrashid007@gmail.com>
2019-10-31 14:13:30 +0600

    connect remort database on mongodb atlas with mongoose

--- app.js
+++ app.js
@@ -1,12 +1,34 @@
 const express = require('express');
 const morgan = require('morgan');
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 
 dotenv.config({ path: './config.env' });
 
 const app = express();
 //Middleware
 app.use(morgan('dev'));
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+//connect database
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  })
+  .then(() => console.log('DB connect successful!'))
+  .catch(err => console.log('DB connection failed!'));
+
+app.get('/api/v1/users', (req, res) => {
+  res.status(200).json({
+    status: 'success'
+  });
+});
 
 //Start the server
 const port = process.env.PORT || 3000;
